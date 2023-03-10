0b24c5f958a3f4871a5512b8a5492be0286f6cb0
Mathurin Dorel <dorel@molgen.mpg.de>
2023-03-10 15:10:44 +0100

    Fix plotKEGG and add control over the number of terms plotted

--- R/kegg.R
+++ R/kegg.R
@@ -1,17 +1,25 @@
 # Functions to display enrichment results from clusterProfiler
 # Can be GO or KEGG output
 
+#' @import forecats
+
 #' Plot KEGG result
 #'
 #' Compute and plots the rich factor for the enriched pathways or terms.
 #' @param kegg_result Output of enrichKEGG or enrichGO
-#' @value The result as a tibble, with an extra column richFactor
-plotKEGG <- function(kegg_result) {
+#' @param max_terms The maximum number of terms to display on the plot
+#' @value The input with an extra column richFactor
+plotKEGG <- function(kegg_result, max_terms=10) {
     kegg_result %>% mutate(richFactor = Count / as.numeric(sub("/\\d+", "", BgRatio))) -> kegg_tibble
     if (nrow(kegg_tibble)==0) {
         message('No enriched terms, skipping...')
     } else {
-        kegg_tibble %>% ggplot(aes(richFactor, fct_reorder(Description, richFactor))) +geom_segment(aes(xend=0, yend = Description)) +geom_point(aes(color=p.adjust, size = Count))+scale_color_gradientn(colours=c("#f7ca64", "#46bac2", "#7e62a3"),guide=guide_colorbar(reverse=TRUE, order=1))
+        print( kegg_tibble %>% as_tibble %>% arrange(desc(richFactor)) %>% head(max_terms) %>%
+              ggplot(aes(richFactor, fct_reorder(Description, richFactor))) +
+              geom_segment(aes(xend=0, yend = Description)) +
+              geom_point(aes(color=p.adjust, size = Count))+
+              scale_color_gradientn(colours=c("#f7ca64", "#46bac2", "#7e62a3"),guide=guide_colorbar(reverse=TRUE, order=1))
+        )
     }
     invisible(kegg_tibble)
 }
